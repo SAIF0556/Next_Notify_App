28252356ea140f4d4cea0c89c3c76d1b20a34822
SM SAIF <smsaif767@gmail.com>
2024-04-04 19:58:12 +0530

    Final Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+/* eslint-disable */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import withPWA from '@ducanh2912/next-pwa'
+
+const nextConfig = withPWA({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})
+
+export default nextConfig
+
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+// })
+// export default withPWA(nextConfig)
